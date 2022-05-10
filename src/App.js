7ce0c45ad6f96c34f2a2487a7f22f3fbf617e0a7
bmsani <bmsani6@gmail.com>
2022-05-11 02:29:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Pages/Header/Header';
+import Footer from './component/Pages/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './component/Pages/Home/Home';
+import About from './component/Pages/About/About';
+import Appointment from './component/Pages/Appointment/Appointment';
+import Reviews from './component/Pages/Reviews/Reviews';
+import ContactUs from './component/Pages/ContactUs/ContactUs';
+import Login from './component/Pages/Login/Login';
+import Register from './component/Pages/Register/Register';
+import NotFound from './component/Pages/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contactUs' element={<ContactUs></ContactUs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/component/Pages/Appointment/Appointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointment = () => {
+    return (
+        <div>
+            Appointment Pages
+        </div>
+    );
+};
+
+export default Appointment;
